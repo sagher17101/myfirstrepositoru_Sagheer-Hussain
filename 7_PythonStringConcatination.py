4ca5d02f9235d73de400e7f152b3110cfe071633
SAGHEER <sagheerrana17101@gmail.com>
2025-11-27 06:53:13 +0500

    Update 7_PythonStringConcatination.py

--- 7_PythonStringConcatination.py
+++ 7_PythonStringConcatination.py
@@ -1,12 +1,12 @@
 
 # String concatination (space include in length)
-x = "Wajahat"
+x = "Sagheer"
 y = " What are you doing" # space at start
 z = x + y 
 print(z)
 
 # String concatination with space between them
-x = "Wajahat"
+x = "Sagheer"
 y = "What are you doing"
 z = x + " " + y 
 print(z)
